d89cdbc31368fbdf7cac47f80e36abc6dd83c0c7
Izzudin Anuar <izzudinanuar96@gmail.com>
2019-12-06 23:58:37 +1300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="What the hell"/>
   </div>
 </template>
 
@@ -12,6 +12,10 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+
+  mounted() {
+    alert('hello world');
   }
 }
 </script>
