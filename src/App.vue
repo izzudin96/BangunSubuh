bf0e35920e79ba38dfef5d822e80b1024acdde3c
Izzudin Anuar <izzudinanuar96@gmail.com>
2019-12-14 18:02:44 +1300

    Get time

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>{{ currentTime }}</h1>
+    <h1>{{ displayTime }}</h1>
     <input v-on:keyup.enter="getPrayerTime" v-model="location" type="text">
   </div>
 </template>
@@ -17,20 +17,34 @@ export default {
       location: 'Hamilton, New Zealand',
       prayerTimes: null,
       todayPrayerTime: null,
+      tomorrowPrayerTime: null,
       currentTime: null,
     }
   },
 
+  computed: {
+    displayTime: function() {
+      return moment(this.currentTime).format('MMMM Do YYYY, h:mm:ss a')
+    },
+  },
+
   mounted() {
-    this.getPrayerTime();
+    this.setTime();
     this.updateTime();
+    this.getPrayerTime();
   },
 
   methods: {
     getPrayerTime() {
-      axios.get('https://api.aladhan.com/v1/calendarByAddress?address=' + this.location + '&method=2&month=12&year=2019')
+      let method = 2;
+      let date = moment(this.currentTime).format("D");
+      let month = moment(this.currentTime).format("M");
+      let year = moment(this.currentTime).format("Y");
+
+      axios.get('https://api.aladhan.com/v1/calendarByAddress?address=' + this.location + `&method=${method}&month=${month}&year=${year}`)
               .then((response) => {
                 this.prayerTimes = response.data['data'];
+                this.todayPrayerTime = this.prayerTimes[date - 1]['timings'];
               })
               .then(() => {
                 console.log(this.prayerTimes[0]['timings']['Maghrib']);
@@ -41,7 +55,7 @@ export default {
     },
 
     setTime() {
-      this.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+      this.currentTime = moment().format();
     },
 
     updateTime() {
