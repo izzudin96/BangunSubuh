29e1c3ceea5b4046624f3afc223c972f61d406eb
Izzudin Anuar <izzudinanuar96@gmail.com>
2019-12-26 00:10:02 +1300

    Still buggy

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
 <template>
     <div id="app">
-        <h1 class="bg-blue">{{ displayTime }}</h1>
+        <h1>{{ displayTime }}</h1>
         <input v-on:keyup.enter="getPrayerTime" v-model="location" type="text">
         <button @click="playAzan(azanPaths.AZAN1)">Play Azan</button>
         <hr>
         <div v-if="this.todayPrayerTimes">
             Subuh: {{ this.todayPrayerTimes['Fajr'] }} -
-            <span v-if="this.currentTime < this.todayPrayerTimes['Fajr']">
+            <span v-if="fajrShow">
                 {{ parseInt(prayerTimesCountdown['Fajr'].asHours()) }} hours {{ parseInt(prayerTimesCountdown['Fajr'].asMinutes())%60 }} minutes and {{ parseInt(prayerTimesCountdown['Fajr'].asSeconds())%60 }} second
             </span>
         </div>
 
         <div v-if="this.todayPrayerTimes">
             Habis Waktu Subuh: {{ this.todayPrayerTimes['Sunrise'] }} -
-            <span v-if="this.currentTime < this.todayPrayerTimes['Sunrise']">
+            <span v-if="sunriseShow">
                 {{ parseInt(prayerTimesCountdown['Sunrise'].asHours()) }} hours {{ parseInt(prayerTimesCountdown['Sunrise'].asMinutes())%60 }} minutes and {{ parseInt(prayerTimesCountdown['Sunrise'].asSeconds())%60 }} second
             </span>
         </div>
 
         <div v-if="this.todayPrayerTimes">
             Zohor: {{ this.todayPrayerTimes['Dhuhr'] }} -
-            <span v-if="this.currentTime < this.todayPrayerTimes['Dhuhr']">
+            <span v-if="dhuhrShow">
                 {{ parseInt(prayerTimesCountdown['Dhuhr'].asHours()) }} hours {{ parseInt(prayerTimesCountdown['Dhuhr'].asMinutes())%60 }} minutes and {{ parseInt(prayerTimesCountdown['Dhuhr'].asSeconds())%60 }} second
             </span>
         </div>
 
         <div v-if="this.todayPrayerTimes">
             Asar: {{ this.todayPrayerTimes['Asr'] }} -
-            <span v-if="this.currentTime < this.todayPrayerTimes['Asr']">
+            <span v-if="asrShow">
                 {{ parseInt(prayerTimesCountdown['Asr'].asHours()) }} hours {{ parseInt(prayerTimesCountdown['Asr'].asMinutes())%60 }} minutes and {{ parseInt(prayerTimesCountdown['Asr'].asSeconds())%60 }} second
             </span>
         </div>
 
         <div v-if="this.todayPrayerTimes">
             Maghrib: {{ this.todayPrayerTimes['Maghrib'] }} -
-            <span v-if="this.currentTime < this.todayPrayerTimes['Maghrib']">
+            <span v-if="maghribShow">
                 {{ parseInt(prayerTimesCountdown['Maghrib'].asHours()) }} hours {{ parseInt(prayerTimesCountdown['Maghrib'].asMinutes())%60 }} minutes and {{ parseInt(prayerTimesCountdown['Maghrib'].asSeconds())%60 }} second
             </span>
         </div>
 
         <div v-if="this.todayPrayerTimes">
             Isyak: {{ this.todayPrayerTimes['Isha'] }} -
-            <span v-if="this.currentTime < this.todayPrayerTimes['Isha']">
+            <span v-if="ishaShow">
                 {{ parseInt(prayerTimesCountdown['Isha'].asHours()) }} hours {{ parseInt(prayerTimesCountdown['Isha'].asMinutes())%60 }} minutes and {{ parseInt(prayerTimesCountdown['Isha'].asSeconds())%60 }} second
             </span>
         </div>
@@ -65,15 +65,39 @@
                 location: 'Hamilton, New Zealand',
                 prayerTimes: null,
                 todayPrayerTimes: null,
-                tomorrowPrayerTimes: null,
                 currentTime: null,
                 azanPaths: paths,
+                isEndOfDay: false,
             }
         },
 
         computed: {
             displayTime: function () {
                 return moment(this.currentTime).format('MMMM Do YYYY, h:mm:ss a')
+            },
+
+            fajrShow: function() {
+                return this.currentTime.isBefore(this.todayPrayerTimes['Fajr']) || this.isEndOfDay;
+            },
+
+            sunriseShow: function() {
+                return this.currentTime.isBefore(this.todayPrayerTimes['Sunrise']) || this.isEndOfDay;
+            },
+
+            dhuhrShow: function() {
+                return this.currentTime.isBefore(this.todayPrayerTimes['Dhuhr']) || this.isEndOfDay;
+            },
+
+            asrShow: function() {
+                return this.currentTime.isBefore(this.todayPrayerTimes['Asr']) || this.isEndOfDay;
+            },
+
+            maghribShow: function() {
+                return this.currentTime.isBefore(this.todayPrayerTimes['Maghrib']) || this.isEndOfDay;
+            },
+
+            ishaShow: function() {
+                return this.currentTime.isBefore(this.todayPrayerTimes['Isha']) || this.isEndOfDay;
             },
 
             prayerTimesCountdown: function() {
@@ -99,20 +123,23 @@
             window.moment = moment;
             this.setTime();
             this.updateTime();
-            this.getPrayerTime();
+
+            let day = moment(this.currentTime).format("D");
+            let month = moment(this.currentTime).format("M");
+            let year = moment(this.currentTime).format("Y");
+
+            this.getPrayerTime(day, month, year);
         },
 
         methods: {
-            getPrayerTime() {
+            getPrayerTime(day, month, year) {
                 let method = 2;
-                let date = moment(this.currentTime).format("D");
-                let month = moment(this.currentTime).format("M");
-                let year = moment(this.currentTime).format("Y");
 
                 axios.get('https://api.aladhan.com/v1/calendarByAddress?address=' + this.location + `&method=${method}&month=${month}&year=${year}`)
                     .then((response) => {
                         this.prayerTimes = response.data['data'];
-                        this.todayPrayerTimes = this.prayerTimes[date - 1]['timings'];
+                        this.todayPrayerTimes = this.prayerTimes[day - 1]['timings'];
+
                         this.todayPrayerTimes['Asr'] = moment(this.todayPrayerTimes['Asr'], "HH:mm");
                         this.todayPrayerTimes['Dhuhr'] = moment(this.todayPrayerTimes['Dhuhr'], "HH:mm");
                         this.todayPrayerTimes['Fajr'] = moment(this.todayPrayerTimes['Fajr'], "HH:mm");
@@ -128,8 +155,16 @@
                     });
             },
 
+            setTomorrowPrayerTimes() {
+                let day = moment(this.currentTime.add(1, 'days')).format("D");
+                let month = moment(this.currentTime.add(1, 'days')).format("M");
+                let year = moment(this.currentTime.add(1, 'days')).format("Y");
+                this.getPrayerTime(day, month, year);
+                this.isEndOfDay = true;
+            },
+
             setTime() {
-                this.currentTime = moment();
+                this.currentTime = moment().subtract(300, 'minutes');
             },
 
             updateTime() {
@@ -150,6 +185,7 @@
                     this.playAzan(this.azanPaths.AZAN1);
                 } else if(time == this.todayPrayerTimes['Isha'].format("HH:mm:ss")) {
                     this.playAzan(this.azanPaths.AZAN1);
+                    this.setTomorrowPrayerTimes();
                 }
             },
 
